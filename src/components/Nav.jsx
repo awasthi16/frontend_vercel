5331bf8e053503483c8b7937962fa61f2b76f566
durgesh awasthi <133563743+awasthi16@users.noreply.github.com>
2025-06-27 11:28:58 +0530

    new update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { Link} from 'react-router-dom'
 const Nav = () => {
   return (
     <div className='nav'>
-    <h1>Awasthi Tech</h1>
+    <h1>Deep Data India Research Centre</h1>
     <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
