babf9dba72ce1bafd427d2bb6d4de12f22fd926f
durgesh awasthi <133563743+awasthi16@users.noreply.github.com>
2025-06-28 07:57:56 +0530

    add css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,44 @@
-import React from 'react'
-import { Link} from 'react-router-dom'
+// import React from 'react'
+// import { Link} from 'react-router-dom'
+
+// const Nav = () => {
+//   return (
+//     <div className='nav'>
+//     <h1>we are superhuman </h1>
+//     <ul>
+//         <li><Link to="/">Home</Link></li>
+//         <li><Link to="/about">About</Link></li>
+//         <li><Link to="/contact">Contact</Link></li>
+//         <li><Link to="/quiz">Quiz</Link></li>
+//         <li><Link to="/signin">SignIn</Link></li>
+//         <li><Link to="/signup">Signup</Link></li>
+//          <li><Link to="/python">python</Link></li>
+
+//     </ul>
+//     </div>
+//   )
+// }
+
+// export default Nav
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Nav.css'; // CSS file for styling
 
 const Nav = () => {
   return (
-    <div className='nav'>
-    <h1>Mayank Mandole The Coder</h1>
-    <ul>
+    <nav className="nav-container">
+      <div className="logo">🚀 Superhuman</div>
+      <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
         <li><Link to="/quiz">Quiz</Link></li>
         <li><Link to="/signin">SignIn</Link></li>
         <li><Link to="/signup">Signup</Link></li>
-         <li><Link to="/python">python</Link></li>
+        <li><Link to="/python">Python</Link></li>
+      </ul>
+    </nav>
+  );
+};
 
-    </ul>
-    </div>
-  )
-}
-
-export default Nav
+export default Nav;
